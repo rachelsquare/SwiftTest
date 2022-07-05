dd97eec045c8e89e91810f1cd52ad6748b80a54b
Jesse Moore <jmoore@affirma.com>
2022-07-05 11:01:50 -0500

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Affirma Interns on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
